3f2c3815f5f3b013333674d327a150d93403bb8b
cryptina443 <114966830+cryptina443@users.noreply.github.com>
2022-10-19 20:56:49 -0400

    Change output and add comments for readability

--- Income Tax Calculator.py
+++ Income Tax Calculator.py
@@ -1,13 +1,16 @@
-
+#initiating variables that will stay constant
 tax_flat_rate=0.20
 stnd_deduct=10000
 
-
-gross_income=float(input('Enter your gross income to the nearest penny: '))
+#asking for input from the user
+gross_income=float(input('Enter the gross income: '))
 num_dep=int(input('Enter the number of dependents: '))
 
+#calculating the deduction based on dependents and storing it
 dep_deduct= num_dep * 3000
 
+#calculating income tax based on deductions and laws
 income_tax = (gross_income - stnd_deduct - dep_deduct) * tax_flat_rate 
 
+#displaying income tax
 print(f'The income tax is ${income_tax}')
